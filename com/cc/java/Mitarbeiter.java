7ceb754700c7336797846f699e806a3e91952176
ASK-USE <knueppelalex@web.de>
2023-04-21 14:52:12 +0200

    1. Save 210423

---
+++ com/cc/java/Mitarbeiter.java
@@ -0,0 +1,20 @@
+package com.cc.java;
+
+public class Mitarbeiter {
+    
+    private String name;
+    private String firstName;
+    private String role;
+    private int entryYear;
+
+      public Mitarbeiter(String name, String firstName, String role, int entryYear) {
+        this.name = name;
+        this.firstName = firstName;
+        this.role = role;
+        this.entryYear = entryYear;
+      }
+
+      void getInfo(Mitarbeiter mitarbeiter)
+      
+
+}
